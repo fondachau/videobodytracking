92bfc325a02faeeeb0135031d02047710a580362
fondachau <chau.fonda@gmail.com>
2018-11-22 12:36:40 -0800

    nov22 1236

--- video.cpp
+++ video.cpp
@@ -6,8 +6,10 @@
 #include <iostream>
 #include <stdio.h>
 #include <time.h>
-
+#include <chrono>
 #include <windows.h>
+#include <iostream>
+#include <fstream>
  
 using namespace cv;
 using namespace cv::dnn;
@@ -22,104 +24,62 @@ int main(){
 	int hourshere;
 
 		// Specify the paths for the 2 files
+	//string protoFile = "pose_deploy_linevec_faster_4_stages.prototxt";
+	//string weightsFile = "pose_iter_160000.caffemodel";
 	string protoFile = "pose_deploy_linevec_faster_4_stages.prototxt";
 	string weightsFile = "pose_iter_160000.caffemodel";
 
-		// Read the network into Memory
+
+	//start video capture 
 	VideoCapture cap(0);
+	
 	if (!cap.isOpened())
 	{
 		cout << "Error opening video stream" << endl;
 		return -1;
 	}
+
+	//get frame size
 	int inWidth = cap.get(CV_CAP_PROP_FRAME_WIDTH);
 	int inHeight = cap.get(CV_CAP_PROP_FRAME_HEIGHT);
+		//Net net = readNetFromCaffe(protoFile, weightsFile);
 
-		Net net = readNetFromCaffe(protoFile, weightsFile);
-		VideoWriter video("outcpp.avi", CV_FOURCC('M', 'J', 'P', 'G'), 10, Size(inWidth, inHeight));
+	//sample to see how frame per second
+	
+	
+	//set up video file to save recording
+	VideoWriter video("outcpp.avi", CV_FOURCC('M', 'J', 'P', 'G'), 30, Size(inWidth, inHeight));
+	VideoWriter videoout("outcpp1.avi", CV_FOURCC('M', 'J', 'P', 'G'), 30, Size(inWidth, inHeight));
 
+	//take 400 frames and save to outcpp.avi
+	auto start = chrono::steady_clock::now();
 
-		for (int x = 0; x < 100;x++)
+	for (int x = 0; x < 400;x++)
 		{
-			Mat frame;
-			//Mat frame1;
-			// Capture frame-by-frame 
-			cap >> frame;
-			// If the frame is empty, break immediately
-			if (frame.empty())
-				break;
-
-		// 
-		//Mat frame = imread("single.jpg");
-		//Mat frame1 = imread("single.jpg", CV_LOAD_IMAGE_COLOR);
-		
-		// Show our image inside it.
-		
-		// Specify the input image dimensions
-		//int inWidth = 700;
-		//int inHeight = 467;
-		// Prepare the frame to be fed to the network
-			/*
-		Mat inpBlob = blobFromImage(frame, 1.0 / 255, Size(inWidth, inHeight), Scalar(0, 0, 0), false, false);
-
-		// Set the prepared object as the input blob of the network
-		net.setInput(inpBlob);
-		Mat output = net.forward();
-		
-
-		int H = output.size[2];
-		int W = output.size[3];
-		// find the position of the body parts
-		vector<Point> points(15);
-		for (int n = 0; n < 15; n++) { // Probability map of corresponding body's part. 
-			Mat probMap(H, W, CV_32F, output.ptr(0,n)); 
-			Point2f p(-1,-1); 
-			Point maxLoc; 
-			double prob; 
-			minMaxLoc(probMap, 0, &prob, 0, &maxLoc);// if (prob > thresh)
-			
-				p = maxLoc;
-				p.x *= float(inWidth) / W;
-				p.y *= float(inHeight) / H;
-				printf("%lf %lf\n", p.x, p.y);
-				circle(frame1,p,5,Scalar(0, 0, 255),-1,8);
-
-			points[n] = p;
-		}
-		*/
-		// Write the frame into the file 'outcpp.avi'
-		video.write(frame);
+		Mat frame1;
+		//Mat frame1;
+		// Capture frame-by-frame 
+		cap >> frame1;
+		// If the frame is empty, break immediately
+		//if (frame1.empty())
+		//	break;
 
 		// Display the resulting frame    
-		imshow("Frame", frame);
-		time1 = (int)time(NULL);
-		seconds = time1 % 60;
-		time1 /= 60;
+		//imshow("Frame", frame1);
 
-		minutes = time1 % 60;
-		time1 /= 60;
-
-		hours = time1 % 24;
-		time1 /= 24;
-
-		hourshere = hours - 8;
-		if (hourshere < 0) {
-			hourshere = 24 + hourshere;
+		video.write(frame1);
 		}
-
-		Sleep(100);
-		printf("H%d M%d  S%d  \n", hourshere, minutes, seconds);
-		// Press  ESC on keyboard to  exit
-		char c = (char)waitKey(1);
-		if (c == 27)
-			break;
-		}
+	auto end = chrono::steady_clock::now();
+	cout << "durations" << chrono::duration_cast<chrono::milliseconds> (end - start).count() << "\n";
+	double fps = 400 / (double)chrono::duration_cast<chrono::milliseconds> (end - start).count() * 1000;
+	cout << "fps" << fps << "\n";
 
 		// When everything done, release the video capture and write object
 		cap.release();
 		video.release();
 
 
+		//now read saved recording
 		VideoCapture cap1("outcpp.avi");
 
 		// Check if camera opened successfully
@@ -127,9 +87,175 @@ int main(){
 			cout << "Error opening video stream or file" << endl;
 			return -1;
 		}
-		VideoWriter video1("outcpp1.avi", CV_FOURCC('M', 'J', 'P', 'G'), 10, Size(inWidth, inHeight));
+		//VideoWriter video1("outcpp1.avi", CV_FOURCC('M', 'J', 'P', 'G'), 10, Size(inWidth, inHeight));
 
 
+		Mat flow, frame;
+		// some faster than mat image container
+		UMat  flowUmat, prevgray;
+		float xnet;
+		float ynet;
+		
+		//output net motion to outnet.txt
+		ofstream myfile1;
+		myfile1.open("outnet.txt");
+		float zero = 0.0;
+		myfile1 << (float)fps << " " << zero << "\n";
+		Net net = readNetFromCaffe(protoFile, weightsFile);
+
+		int toprange = 300;
+		int bottomrange = 301;
+		int leftrange = 200;
+		int rightrange = 400;
+		//look at each frame and compare it to previous frame
+		for (;;)
+		{
+			bool Is = cap1.grab();
+			if (Is == false) {
+				// if video capture failed
+				//cout << "Video Capture Fail" << endl;
+				break;
+			}
+			else {
+
+				float positionx=0;
+				float positiony=0;
+
+				Mat img;
+				Mat original;
+				
+				// capture frame from video file
+				cap1.retrieve(img, CV_CAP_OPENNI_BGR_IMAGE);
+
+				resize(img, img, Size(640, 480));
+
+				// save original for later
+				img.copyTo(original);
+
+				// just make current frame gray
+				cvtColor(img, img, COLOR_BGR2GRAY);
+
+
+				if (prevgray.empty() == false) {
+
+					// calculate optical flow 
+					calcOpticalFlowFarneback(prevgray, img, flowUmat, 0.4, 1, 12, 2, 8, 1.2, 0);
+					// copy Umat container to standard Mat
+					flowUmat.copyTo(flow);
+					xnet = 0;
+					ynet = 0;
+
+					// By y += 10, x += 10 you can specify the grid 
+					//location of interest
+					for (int y = toprange; y < bottomrange; y += 10) {
+						for (int x = leftrange; x < rightrange; x += 10)
+						{
+							// get the flow from y, x position * 10 for better visibility
+							const Point2f flowatxy = flow.at<Point2f>(y, x) * 10;
+							// draw line at flow direction
+							line(original, Point(x, y), Point(cvRound(x + flowatxy.x), cvRound(y + flowatxy.y)), Scalar(255, 0, 0));
+							// draw initial point
+							circle(original, Point(x, y), 1, Scalar(0, 0, 0), -1);
+							xnet += flowatxy.x;//net posion of desired area
+							ynet += flowatxy.y;
+						}
+					}
+				
+					positionx += xnet;
+					positiony += ynet;
+					myfile1 << xnet << " " << ynet << "\n";
+					// draw the results
+					namedWindow("prew", WINDOW_AUTOSIZE);
+					imshow("prew", original);
+
+					// fill previous image again
+					img.copyTo(prevgray);
+					videoout.write(original);
+				}
+				else {
+
+					toprange = 300;
+					bottomrange = original.rows;
+					leftrange = 200;
+					rightrange = 400;
+					printf("1111111");
+					/*
+					
+					Implement openpose
+					HERE
+					get points of shoulders
+
+
+					
+					
+					printf("\n 0");
+					Mat frame1=original;
+					Mat inpBlob = blobFromImage(frame1, 1.0 / 255, Size(inWidth, inHeight), Scalar(0, 0, 0), false, false);
+
+					// Set the prepared object as the input blob of the network
+					net.setInput(inpBlob);
+					Mat output = net.forward();
+
+
+					int H = output.size[2];
+					int W = output.size[3];
+					// find the position of the body parts
+					vector<Point> points(15);
+					for (int n = 0; n < 15; n++) { // Probability map of corresponding body's part. 
+						Mat probMap(H, W, CV_32F, output.ptr(0, n));
+						Point2f p(-1, -1);
+						Point maxLoc;
+						double prob;
+						minMaxLoc(probMap, 0, &prob, 0, &maxLoc);// if (prob > thresh)
+
+						p = maxLoc;
+						p.x *= float(inWidth) / W;
+						p.y *= float(inHeight) / H;
+						printf("%lf %lf\n", p.x, p.y);
+						circle(frame1, p, 5, Scalar(0, 0, 255), -1, 8);
+						points[n] = p;
+					}
+					imshow("refrence", frame1);
+					printf("1");*/
+					// fill previous image in case prevgray.empty() == true
+					img.copyTo(prevgray);
+
+				}
+
+
+				int key1 = waitKey(20);
+				
+			}
+		}
+		auto last = chrono::steady_clock::now();
+		cout << chrono::duration_cast<chrono::milliseconds> (last - start).count() << "\n";
+
+		cap1.release();
+		videoout.release();
+
+		myfile1.close();
+}
+
+/*				time1 = (int)time(NULL);
+				seconds = time1 % 60;
+				time1 /= 60;
+
+				minutes = time1 % 60;
+				time1 /= 60;
+
+				hours = time1 % 24;
+				time1 /= 24;
+
+				hourshere = hours - 8;
+				if (hourshere < 0) {
+					hourshere = 24 + hourshere;
+				}
+				printf("H%d M%d  S%d  \n", hourshere, minutes, seconds);
+				*/
+
+
+
+		/*
 		while (1) {
 
 			Mat frame;
@@ -176,7 +302,6 @@ int main(){
 			if (c == 27)
 				break;
 		}
-
 		// When everything done, release the video capture object
 		cap1.release();
 
@@ -189,6 +314,54 @@ int main(){
 		
 		
 }
+	*/
+
+
+
+
+
+
+// 
+		//Mat frame = imread("single.jpg");
+		//Mat frame1 = imread("single.jpg", CV_LOAD_IMAGE_COLOR);
+
+		// Show our image inside it.
+
+		// Specify the input image dimensions
+		//int inWidth = 700;
+		//int inHeight = 467;
+		// Prepare the frame to be fed to the network
+			/*
+		Mat inpBlob = blobFromImage(frame, 1.0 / 255, Size(inWidth, inHeight), Scalar(0, 0, 0), false, false);
+
+		// Set the prepared object as the input blob of the network
+		net.setInput(inpBlob);
+		Mat output = net.forward();
+
+
+		int H = output.size[2];
+		int W = output.size[3];
+		// find the position of the body parts
+		vector<Point> points(15);
+		for (int n = 0; n < 15; n++) { // Probability map of corresponding body's part.
+			Mat probMap(H, W, CV_32F, output.ptr(0,n));
+			Point2f p(-1,-1);
+			Point maxLoc;
+			double prob;
+			minMaxLoc(probMap, 0, &prob, 0, &maxLoc);// if (prob > thresh)
+
+				p = maxLoc;
+				p.x *= float(inWidth) / W;
+				p.y *= float(inHeight) / H;
+				printf("%lf %lf\n", p.x, p.y);
+				circle(frame1,p,5,Scalar(0, 0, 255),-1,8);
+
+			points[n] = p;
+		}
+		*/
+
+
+
 
 
 	/*
